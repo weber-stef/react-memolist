51f3fe1e4af8881e331d1ea3946a18dbe93d155c
Stefan Wurck <vitatech@gmx.de>
2019-04-03 13:47:12 +0200

    Updates

---
+++ precache-manifest.ac304b5e9f985f963adddbbcc998e190.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-memolist/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3738d567889df8ef8308",
+    "url": "/react-memolist/static/js/runtime~main.cb4315b7.js"
+  },
+  {
+    "revision": "814bc5c6828e4dcc2443",
+    "url": "/react-memolist/static/js/main.a0917801.chunk.js"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/react-memolist/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "814bc5c6828e4dcc2443",
+    "url": "/react-memolist/static/css/main.3f6323b8.chunk.css"
+  },
+  {
+    "revision": "dd3cd458fd1434cc6df09de03c9c5100",
+    "url": "/react-memolist/index.html"
+  }
+];
